ffa05c8c8069699a41e9de59000e4518d981f6b1
Ryan Maybury <ryan2711@hotmail.co.uk>
2023-05-16 14:52:24 +0100

    Added more theme options

--- lua/rshop/config/cl_theme.lua
+++ lua/rshop/config/cl_theme.lua
@@ -1,5 +1,13 @@
 RShop.Theme = {
     body = Color(40, 45, 56),
+
+    primary = Color(45, 51, 63),
+
+    secondary = Color(31, 35, 43),
+
+    red = Color(224, 67, 67),
     
-    text = Color(200, 200, 200)
+    text = Color(200, 200, 200),
+
+
 }
